6aca3f96ac9bbf3f5566b2ce9eb11095f1bdf724
elvisanthonyy <info.elvisanthony@gmail.com>
2025-09-29 14:16:14 +0100

    animation

--- app/component/Main.tsx
+++ app/component/Main.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import {
   FaInstagram,
   FaLinkedin,
@@ -7,23 +8,82 @@ import {
   FaLocationArrow,
 } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
+import { FaAngleUp } from "react-icons/fa";
 
 import Skill from "./Skill";
 
+function useScrollAnimation() {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isVisible, setIsVisble] = useState(false);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const [entry] = entries;
+        if (entry.isIntersecting) {
+          setIsVisble(true);
+        }
+      },
+      { threshold: 0.2 }
+    );
+
+    if (ref.current) observer.observe(ref.current);
+
+    return () => {
+      if (ref.current) observer.unobserve(ref.current);
+    };
+  }, []);
+
+  return { ref, isVisible };
+}
+
 const Main = () => {
+  const item1 = useScrollAnimation();
+  const item2 = useScrollAnimation();
+  const item3 = useScrollAnimation();
+  const item4 = useScrollAnimation();
+  const projectRef = useRef<HTMLDivElement>(null);
+  const topRef = useRef<HTMLDivElement>(null);
+  const [isButtonsVisible, setIsButtonVisble] = useState(false);
+  const handleScrollToProject = () => {
+    projectRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+  const handleScrollToTop = () => {
+    topRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    setIsButtonVisble(true);
+  });
+
   return (
-    <main className="flex flex-col items-center relative pb-50 min-h-screen h-fit">
-      <div className="flex pt-30 h-screen flex-col w-full items-center bg-gradient-to-b from-white/0 from-10% via-green-400/10 to-white/0">
-        <div className="text-3xl mt-10 text-gray-300">Hello</div>
-        <div className="text-3xl mt-1 ">
-          I&apos;m <span className="text-yellow-500 text-4xl">Elvis</span>
+    <main
+      ref={topRef}
+      className="flex flex-col items-center relative pb-50 min-h-screen h-fit"
+    >
+      <div className="flex pt-25 h-screen flex-col w-full items-center bg-gradient-to-b from-white/0 from-10% via-green-400/10 to-white/0">
+        <div className="text-2xl mt-5 mb-2 text-gray-300">
+          <div className="mr-2">
+            Hello, I&apos;m{" "}
+            <span className="text-yellow-500 text-2xl">Elvis</span>
+          </div>
         </div>
-        <div className="text-3xl mt-1 text-gray-300">Web Developer</div>
-        <div className="flex justify-center  w-full mt-5">
-          <button className="cursor-pointer transition-all duration-500 text-sm flex m-3 rounded-4xl justify-center items-center w-[35%] h-12 bg-white/5 border-1 border-white text-white hover:bg-white hover hover:text-black">
+        <div className="text-4xl mt-1 text-gray-300 mb-3">Web Developer</div>
+        <div className="flex text-center text-xl mt-1 text-gray-300 mb-5 w-[85%]">
+          Crafting digital solutions, one pixel at a time
+        </div>
+
+        <div
+          className={`transition-all duration-700 ease-in flex justify-center ${
+            isButtonsVisible ? "opacity-100" : "opacity-0"
+          }  w-full mt-5`}
+        >
+          <button className="cursor-pointer transition-all duration-500 text-sm flex m-3 rounded-4xl justify-center items-center w-[35%] h-15 bg-white/5 border-1 border-white text-white hover:bg-white hover hover:text-black">
             Hire me
           </button>
-          <button className="cursor-pointer transition-all duration-500 text-sm flex m-3 rounded-4xl justify-center items-center w-[35%] h-12 bg-green-700/10 border-1 border-green-500 hover:bg-green-500">
+          <button
+            onClick={handleScrollToProject}
+            className="cursor-pointer transition-all duration-500 text-sm flex m-3 rounded-4xl justify-center items-center w-[35%] h-15 bg-green-700/10 border-1 border-green-500 hover:bg-green-500"
+          >
             Projects
           </button>
         </div>
@@ -34,11 +94,20 @@ const Main = () => {
               src="/elvis2.png"
               className="h-500 -mb-20 object-contain"
               alt="my image"
+              width={500}
+              height={500}
             />
           </div>
         </div>
       </div>
-      <div className="aspect-square  w-full border-t-1 border-b-1 bg-gradient-to-bl to-50% from-white/3 to-green-300/2  mx-auto flex flex-col justify-start items-center py-10 border-green-600">
+      <div
+        ref={item1.ref}
+        className={`transition-all duration-700 ease-in transform aspect-square  w-full border-t-1 border-b-1 bg-gradient-to-bl to-50% from-white/3 ${
+          item1.isVisible
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 translate-y-10"
+        } to-green-300/2  mx-auto flex flex-col justify-start items-center py-10 border-green-600`}
+      >
         <div className="mb-5">About</div>
         <div className="w-[80%] flex justify-center items-center">
           I&apos;m Elvis Anthony, a passionate full-stack web develover that
@@ -47,7 +116,14 @@ const Main = () => {
       </div>
       <div className="flex flex-col mx-auto my-10">Skills</div>
 
-      <div className="aspect-square w-[90%] bg-gradient-to-bl to-50% from-white/3 to-green-300/2 border-1 mx-auto flex flex-col justify-start py-10 items-center rounded-3xl border-gray-400">
+      <div
+        ref={item2.ref}
+        className={`transition-all duration-700 ease-in aspect-square w-[90%] bg-gradient-to-bl ${
+          item2.isVisible
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 translate-y-10"
+        } to-50% from-white/3 to-green-300/2 border-1 mx-auto flex flex-col justify-start py-10 items-center rounded-3xl border-gray-400`}
+      >
         <div className="mb-5">FRONTEND</div>
         <Skill name={"HTML"} progress={100} />
         <Skill name={"CSS"} progress={75} />
@@ -86,7 +162,9 @@ const Main = () => {
           <div> info.elvisanthony@gmail.com </div>
         </div>
       </div>
-      <div className="flex flex-col mx-auto my-10">MY PROJECTS</div>
+      <div ref={projectRef} className="flex flex-col mx-auto my-10">
+        MY PROJECTS
+      </div>
       <div className="relative aspect-[4/5] px-3 overflow-hidden w-[90%] border-1 bg-gradient-to-bl to-50% from-white/3 to-green-300/2 bg-white/2 shadow-3xl rounded-2xl ">
         <div className="absolute top-0 left-0 w-full h-[40%] bg-white"></div>
         <div className="text-xl mt-[60%] text-green-500 m-3">Lyric Church</div>
@@ -113,6 +191,12 @@ const Main = () => {
             Code
           </div>
         </div>
+      </div>
+      <div
+        onClick={handleScrollToTop}
+        className="fixed right-10 bottom-10 flex justify-center items-center cursor-pointer w-12 h-12 rounded-full border-1 border-white/50 text-white bg-green-800/90 shadow-4xl backdrop-blur-2xl"
+      >
+        <FaAngleUp />
       </div>
     </main>
   );
--- app/component/Nav.tsx
+++ app/component/Nav.tsx
@@ -3,7 +3,7 @@ import { MdMenu } from "react-icons/md";
 
 const Nav = () => {
   return (
-    <nav className="flex bg-white/0 backdrop-blur-xl text-md justify-between items-center z-10 px-[5%] fixed top-0 left-0 w-full h-18">
+    <nav className="flex bg-white/0 backdrop-blur-xl text-2xl justify-between items-center z-10 px-[5%] fixed top-0 left-0 w-full h-18">
       <div>Elvis Anthony</div>
       <MdMenu className="text-2xl" />
     </nav>
