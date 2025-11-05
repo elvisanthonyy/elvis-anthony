52de81182b3f22bc259305fd4414ccce0498acb2
elvisanthonyy <info.elvisanthony@gmail.com>
2025-11-05 23:39:29 +0100

    contact

--- app/component/Nav.tsx
+++ app/component/Nav.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 interface ChildProps {
-  contact: () => void;
+  contact?: () => void;
 }
 
 const Nav = ({ contact }: ChildProps) => {
@@ -10,7 +10,7 @@ const Nav = ({ contact }: ChildProps) => {
     <nav className="flex absolute top-5 left-[50%] -translate-x-[50%] justify-between rounded-2xl items-center z-10 pl-[5%] w-[90%] md:w-[60%] h-11 md:h-13 bg-nav-gray">
       <div className="text-sm serif-font">Elvis Anthony</div>
       <button
-        onClick={() => contact()}
+        onClick={contact}
         className="h-full w-27 rounded-2xl bg-light-black text-sm text-white"
       >
         Contact Me
