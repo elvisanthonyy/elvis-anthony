1db1ad92a975567ef0ca79e264a29074bc86292e
elvisanthonyy <info.elvisanthony@gmail.com>
2025-11-14 13:00:17 +0100

    Fix closing div tag in Main.tsx

--- app/component/Main.tsx
+++ app/component/Main.tsx
@@ -175,7 +175,8 @@ const Main = () => {
             I&apos;m Elvis, a <b>full-stack web developer based</b> in Abuja,
             Nigeria. I build apps end-to-end-clean code, clear and fast
             delivery. My focus: performance, accessibility, and scalability.
-         <div
+          </div>
+            <div
             className={`flex items-center text-sm ${
               theme === "light" ? "text-lighter-black" : "text-darkGray"
             }  px-4 md:px-6 md:mt-4 w-full h-10`}
